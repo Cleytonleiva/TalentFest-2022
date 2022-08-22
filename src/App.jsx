4f17c724e87493708ee4a73e5e90a2e48fd3bcde
genesis <gnsslr@gmail.com>
2022-08-22 16:29:02 -0400

    Getting product data

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import {ProductProvider} from './context/productContext'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>HOLA TALENT FEST</h1>
+      <ProductProvider />
     </div>
 
   )
